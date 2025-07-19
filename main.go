1ccb4ed5c207b2fc2049267e121d20c62ff4c2f4
James-Lu-none <jamesabcde277@gmail.com>
2025-07-19 11:38:16 +0800

    update import

--- main.go
+++ main.go
@@ -1,13 +1,12 @@
 package main
 
+import "start-limit-scheduler/limiter"
 import (
     "os"
 
     "k8s.io/kubernetes/cmd/kube-scheduler/app"
-    "github.com/yourrepo/scheduler/limiter"
-    "k8s.io/component-base/configz"
-    "k8s.io/kubernetes/pkg/scheduler/framework/runtime"
 )
+
 
 func main() {
     command := app.NewSchedulerCommand(
